1736e67af6b7c986635b23717bd233ccf1d23aec
Talgat Nariev <entezeer@MacBook-Pro-Talgat.local>
2020-09-22 14:29:21 +0600

    change server for celery worker

--- api/tasks.py
+++ api/tasks.py
@@ -1,3 +1,5 @@
+from celery import Celery
+from celery.task import periodic_task
 from datetime import timedelta
 from os import environ
 from time import sleep
@@ -5,24 +7,16 @@ from time import sleep
 from . import parse_world, parse_kg
 from .models import News
 import celery
+
 app = celery.Celery('dobush')
 
 REDIS_URL = environ.get('REDISTOGO_URL', 'redis://localhost')
 
 # Use Redis as our broker and define json as the default serializer
-app.conf.update(
-    BROKER_URL=REDIS_URL,
-    CELERY_TASK_SERIALIZER='json',
-    CELERY_ACCEPT_CONTENT=['json'],
-    CELERYBEAT_SCHEDULE = {
-        'get_news': {
-            'task': 'tasks.create_request',
-            'schedule': timedelta(minutes=2)
-        },
-    }
-)
+celery = Celery('tasks', broker=REDIS_URL)
 
-@celery.task
+
+@periodic_task(run_every=timedelta(minutes=3))
 def create_request():
     News.objects.all().delete()
     # parse_world.lenta()
@@ -30,7 +24,6 @@ def create_request():
     parse_world.bbc()
     parse_kg.getNews()
     # sleep(3600)
-
 
 # while True:
 #     create_response()
