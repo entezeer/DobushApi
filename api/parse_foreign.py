6bc1436e65cb20e35dcc16ab622e782253e6cab9
Talgat Nariev <entezeer@MacBook-Pro-Talgat.local>
2020-10-09 21:06:59 +0600

    add foreign news

--- api/parse_foreign.py
+++ api/parse_foreign.py
@@ -17,17 +17,20 @@ headers = {
 }
 session = requests.session()
 
-# def getNews():
-#     News.objects.filter(category=Category.objects.get(name='Иностранные')).all().delete()
+def getNews():
+    News.objects.filter(category=Category.objects.get(name='Иностранные')).all().delete()
+    getEuronewsNews()
+    getCinHuaNews()
+    getArabNews()
+    getCnnNews()
 
 def getArabNews():
     response = requests.get('https://www.alarabiya.net/latest-news', headers=headers)
     bs = BeautifulSoup(response.content, 'lxml')
-    ar = bs.select('ul#Scroll li a[href]')[0:10]
+    ar = bs.select('ul#Scroll li a[href]')[0:20]
     newAr = [v for k, v in enumerate(ar) if not k % 2]
     for a in newAr:
         try:
-            print(a['href'])
             response_ = requests.get('https://www.alarabiya.net' + a['href'], headers=headers)
             soup = BeautifulSoup(response_.content, 'lxml')
             title = soup.find('h1').text
@@ -49,7 +52,125 @@ def getArabNews():
                     author='www.alarabiya.net',
                     img=img,
                     category=Category.objects.get(name='Иностранные'),
-                    language=""
+                    language=3
+                )
+            except:
+                print()
+
+        except:
+            print()
+
+def getEuronewsNews():
+    response = requests.get('https://www.euronews.com/news/international', headers=headers)
+    bs = BeautifulSoup(response.content, 'lxml')
+    for a in bs.select('div.o-block-more-news-themes__articles div.m-object__body h3 a')[0:10]:
+        try:
+            response_ = requests.get('https://www.euronews.com' + a['href'], headers=headers)
+            soup = BeautifulSoup(response_.content, 'lxml')
+            title = soup.find('h1', class_='c-article-title u-display-inline u-text-align--start').text
+
+            try:
+                img = soup.find('img', class_='c-article-media__img').get('src')
+            except:
+                img = None
+
+            content = soup.select('div.c-article-content')
+            try:
+                News.objects.create(
+                    title=title,
+                    content=str(content),
+                    url='https://www.euronews.com' + a['href'],
+                    author='www.euronews.com',
+                    img=img,
+                    category=Category.objects.get(name='Иностранные'),
+                    language=1
+                )
+            except:
+                print()
+
+        except:
+            print()
+
+def getCnnNews():
+    response = requests.get('https://edition.cnn.com/world', headers=headers)
+    bs = BeautifulSoup(response.content, 'lxml')
+    for a in bs.select('h3.cd__headline a')[0:10]:
+        try:
+            response_ = requests.get('https://edition.cnn.com' + a['href'], headers=headers)
+            soup = BeautifulSoup(response_.content, 'lxml')
+            title = soup.find('h1', class_='pg-headline').text
+
+            content = soup.select('div.l-container')
+
+            try:
+                News.objects.create(
+                    title=title,
+                    content=str(content),
+                    url='https://edition.cnn.com' + a['href'],
+                    author='edition.cnn.com',
+                    img=None,
+                    category=Category.objects.get(name='Иностранные'),
+                    language=1
+                )
+            except:
+                print()
+
+        except:
+            print()
+
+
+def getCinHuaNews():
+    response = requests.get('http://www.xinhuanet.com/world/', headers=headers)
+    bs = BeautifulSoup(response.content, 'lxml')
+    for a in bs.select('li.clearfix h3 a')[0:10]:
+        try:
+            print(a['href'])
+            response_ = requests.get(a['href'], headers=headers)
+            soup = BeautifulSoup(response_.content, 'lxml')
+            title = soup.find('div', class_='h-title').text
+            print(title)
+
+            content = soup.select('div#p-detail')
+            print(content)
+            try:
+                News.objects.create(
+                    title=title,
+                    content=str(content),
+                    url=a['href'],
+                    author='www.xinhuanet.com',
+                    img=None,
+                    category=Category.objects.get(name='Иностранные'),
+                    language=2
+                )
+            except:
+                print()
+
+        except:
+            print()
+
+def getWorldJournalNews():
+    response = requests.get('https://www.worldjournal.com/wj/cate/breaking',
+                            headers=headers)
+    bs = BeautifulSoup(response.content, 'lxml')
+    ar = bs.select('div.subcate-list__link__text a[href]')[0:20]
+    newAr = [v for k, v in enumerate(ar) if k % 2]
+    for a in newAr:
+        try:
+            response_ = requests.get(a['href'], headers=headers)
+            soup = BeautifulSoup(response_.content, 'lxml')
+            title = soup.find('h1', class_='article-content__title').text
+
+            content = soup.select('section.article-content__editor')
+
+            try:
+                News.objects.create(
+                    title=title,
+                    content=str(content),
+                    url=a['href'],
+                    author='www.worldjournal.com',
+                    img=None,
+                    category=Category.objects.get(name='Иностранные'),
+                    language=2
                 )
             except:
                 print()
