e8a74f0da66cb778d4ed620a454c5be645f06ed5
Talgat Nariev <entezeer@MacBook-Pro-Talgat.local>
2020-11-12 19:35:29 +0600

    add poll

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Source, News
+from .models import Category, Source, News, Poll, Choice
 
 admin.site.register(Category)
 admin.site.register(Source)
 admin.site.register(News)
+admin.site.register(Poll)
+admin.site.register(Choice)
