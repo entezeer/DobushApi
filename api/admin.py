857da26c9ca28f4121e876dae8565f4211e359eb
Talgat Nariev <entezeer@MacBook-Pro-Talgat.local>
2020-11-18 21:29:52 +0600

    add polls

--- api/admin.py
+++ api/admin.py
@@ -6,5 +6,17 @@ from .models import Category, Source, News, Poll, Choice
 admin.site.register(Category)
 admin.site.register(Source)
 admin.site.register(News)
-admin.site.register(Poll)
-admin.site.register(Choice)
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Poll, QuestionAdmin)
