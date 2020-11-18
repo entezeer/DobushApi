8a6253d3321af0a0f83738b6a11cd0754de58718
Talgat Nariev <entezeer@MacBook-Pro-Talgat.local>
2020-11-18 22:28:50 +0600

    add polls

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from .models import News
+from .models import News, Poll
+
+class PollsSerializer(serializers.ModelSerializer):
+    choices = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Poll
+        fields = '__all__'
+        depth = 1
 
 
 class NewsSerializer(serializers.ModelSerializer):
-    choices = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    polls = PollsSerializer(many=True)
 
     class Meta:
         model = News
